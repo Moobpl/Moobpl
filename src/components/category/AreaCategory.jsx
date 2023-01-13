6e5a17e9bb535da9ecb8e61284dd44efb6576409
bigbit99 <108947985+bigbit99@users.noreply.github.com>
2023-01-13 11:02:27 +0900

    지역 선택

--- src/components/category/AreaCategory.jsx
+++ src/components/category/AreaCategory.jsx
@@ -1,7 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate} from "react-router-dom";
+import styled from "styled-components";
+import { AREACATEGORIES } from "./AREACATEGORIES";
 
-const AreaCategory = () => {
-  return <div>AreaCategory</div>;
+const AreaCategory = ({ list, closeCategory, categoryAnimation }) => {
+  const [currentCity, setCurrentCity] = useState(1);
+
+  const subCategories = AREACATEGORIES.find(
+    category => category.id === currentCity
+  ).subcategories;
+
+  const navigate = useNavigate();
+
+  const moveToCategory = id => {
+    navigate(`/${id}`);
+  }
+
+  return (
+    <S.AreaCategory>
+      <S.Cities>
+        {AREACATEGORIES.map(city => {
+          return(
+            <div
+              key={city.id}
+              onMouseEnter={() => setCurrentCity(city.id)}
+              isSelected={currentCity === city.id}
+            >
+              <span>{city.name}</span>
+              {currentCity === city.id && (
+                <span>
+                </span>
+              )}
+            </div>
+          );
+        })
+        }
+      </S.Cities>
+      <S.SubCategories>
+        {subCategories.map(subCategory => {
+          return(
+            <div
+              key={subCategory.subCategoryId}
+              isSelected={parseInt(list) === subCategory.subCategoryId}
+              onClick={() => moveToCategory(subCategory.subCategoryId)}
+            >
+              {subCategory.name}
+            </div>
+          );
+        })}
+      </S.SubCategories>
+    </S.AreaCategory>
+  );
 };
+
+const S = {
+  AreaCategory: styled.div`
+    height: 555px;
+    background-color: #fbfbfc;
+    border: none;
+    color: black;
+  `,
+  Cities: styled.ul`
+    width: 168px;
+    height: 100%;
+    padding: 23px 10px 0 36px;
+  `,
+  SubCategories: styled.ul`
+    width: 302px;
+    height: 100%;
+    padding: 23px 0 0 36px;
+  `,
+  Category: styled.li`
+    width: 100%;
+    height: 27px;
+    margin-bottom: 5px;
+    font-size: 13px;
+    font-weight: 100;
+    cursor: pointer;
+  `,
+};
+
 
 export default AreaCategory;
--- src/pages/MoobAdd.jsx
+++ src/pages/MoobAdd.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import AreaCategory from "../components/category/AreaCategory";
 
-function MoobAdd() {
-  return <div>MoobAdd</div>;
-}
+
+const MoobAdd = ({ list, city, subCategory }) => {
+  const [isCategoryOpen, setIsCategoryOpen] = useState(true);
+  const [categoryAnimation, setCategoryAnimation] = useState(true);
+
+  const openCategory = () => {
+    setIsCategoryOpen(true);
+  }
+
+  const closeCategory = () => {
+    setCategoryAnimation(false);
+    setTimeout(() => {
+      setIsCategoryOpen(false);
+      setCategoryAnimation(true);
+    }, 100);
+  }
+  return (
+    <>
+      <div className="SelectCategory" 
+        onMouseEnter={openCategory}
+      >
+        <span>{city}</span>
+        <span>{subCategory}</span>
+        {isCategoryOpen && (
+          <AreaCategory 
+            list={list}
+            closeCategory={closeCategory}
+            categoryAnimation={categoryAnimation}
+          />
+        )}
+      </div>
+    </>
+  );
+};
 
 export default MoobAdd;
